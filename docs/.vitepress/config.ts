fe83a5dff95da6030e36891d38acff413ee77560
Atticus6 <94210351+Atticus6@users.noreply.github.com>
2025-01-05 23:07:26 +0800

    添加navbar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,3 +1,5 @@
+import { readdirSync } from 'node:fs'
+import { basename, extname, join } from 'node:path'
 import {
   componentPreview,
   containerPreview,
@@ -12,19 +14,55 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
+      { text: '首页', link: '/' },
+      { text: '组件', link: '/components' },
+      { text: '工具函数', link: '/utils' },
+      { text: 'Hooks', link: '/hooks' },
     ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-        ],
-      },
-    ],
+    search: {
+      provider: 'local',
+    },
+    lastUpdated: {
+      text: '最近更新',
+    },
+    sidebar: {
+      components: [
+        {
+          text: '组件',
+          items: sortFilenames(
+            extractFileNames(getFilesInDirectory('components')),
+          ).map(text => ({
+            text: text === 'index' ? '介绍' : text,
+            link: `/components/${text}`,
+          })),
+          // items: [
+          //   { text: "Markdown Examples", link: "/markdown-examples" },
+          // ],
+        },
+      ],
+      utils: [
+        {
+          text: '工具函数',
+          items: sortFilenames(
+            extractFileNames(getFilesInDirectory('utils')),
+          ).map(text => ({
+            text: text === 'index' ? '介绍' : text,
+            link: `/utils/${text}`,
+          })),
+        },
+      ],
+      hooks: [
+        {
+          text: 'Hooks',
+          items: sortFilenames(
+            extractFileNames(getFilesInDirectory('hooks')),
+          ).map(text => ({
+            text: text === 'index' ? '介绍' : text,
+            link: `/utils/${text}`,
+          })),
+        },
+      ],
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
@@ -37,3 +75,32 @@ export default defineConfig({
     },
   },
 })
+
+function getFilesInDirectory(directoryPath: string) {
+  try {
+    const files = readdirSync(`./docs/${directoryPath}`) // 同步读取文件夹内容
+    const filePaths = files.map(file => join(directoryPath, file)) // 生成完整路径
+    return filePaths
+  }
+  catch (error) {
+    console.error('Error reading directory:', error)
+    return []
+  }
+}
+
+function extractFileNames(filePaths: string[]) {
+  return filePaths.map((filePath) => {
+    const fileName = basename(filePath, extname(filePath)) // 获取文件名并移除扩展名
+    return fileName
+  })
+}
+
+function sortFilenames(fileNames: string[]) {
+  return fileNames.sort((a, b) => {
+    if (a === 'index')
+      return -1 // `index` 总是在最前
+    if (b === 'index')
+      return 1
+    return a.localeCompare(b) // 按字母顺序排序
+  })
+}
