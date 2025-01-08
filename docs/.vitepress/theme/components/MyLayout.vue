f3371780b2b19e16d3290b9c6cfc7f6f50d33c88
wanglt <wanglt@startimes.com.cn>
2025-01-08 10:41:39 +0800

    antd适配适配暗黑模式

--- docs/.vitepress/theme/components/MyLayout.vue
+++ docs/.vitepress/theme/components/MyLayout.vue
@@ -1,30 +1,37 @@
 <script setup lang="ts">
+import { ConfigProvider, theme } from 'ant-design-vue'
+import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Contributors from './Contributors.vue'
+
+const { isDark } = useData()
+const { darkAlgorithm, defaultAlgorithm } = theme
 
 const { Layout } = DefaultTheme
 </script>
 
 <template>
-  <Layout>
-    <!-- 插槽1 -->
-    <template #aside-outline-before>
-      <div class="title">
-        aside-outline-before
-      </div>
-    </template>
+  <ConfigProvider :theme="{ algorithm: isDark ? darkAlgorithm : defaultAlgorithm }">
+    <Layout>
+      <!-- 插槽1 -->
+      <template #aside-outline-before>
+        <div class="title">
+          aside-outline-before
+        </div>
+      </template>
 
-    <!-- 插槽2 -->
-    <template #doc-before>
-      <div class="title">
-        doc-before
-      </div>
-    </template>
+      <!-- 插槽2 -->
+      <template #doc-before>
+        <div class="title">
+          doc-before
+        </div>
+      </template>
 
-    <template #doc-footer-before>
-      <Contributors />
-    </template>
-  </Layout>
+      <template #doc-footer-before>
+        <Contributors />
+      </template>
+    </Layout>
+  </ConfigProvider>
 </template>
 
 <style>
