83ed6a864b4bf8e5c23081364d16f2e10f4b6948
Jove <jove.qaq@qq.com>
2025-01-15 15:43:11 +0800

    feat:时间范围组件

---
+++ docs/components/demo/time_range_model/Base.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TimeRangeModel from '@/components/time_range_model/index.vue'
+import { ref } from 'vue'
+
+const timeRangeData = ref<any>({})
+</script>
+
+<template>
+  <div>
+    <TimeRangeModel
+      v-model:start-date="timeRangeData.startDate"
+      v-model:end-date="timeRangeData.endDate"
+    />
+  </div>
+  {{ timeRangeData }}
+</template>
---
+++ docs/components/time_range_model.md
@@ -0,0 +1,24 @@
+---
+title: TimeRangeModel
+description: 时间范围组件
+author: Jove
+contributors:  []
+---
+
+# 双向绑定数据的时间范围组件
+
+说明: 通过 antd 二次开发(web 端)
+
+## 依赖
+
+```
+npm install moment @ant-design/icons-vue
+```
+
+## 基础用法
+::: preview
+demo-preview=./demo/time_range_model/Base.vue
+:::
+
+## 源代码
+<<< ../../src/components/time_range_model/index.vue
